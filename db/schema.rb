090b936473b51b485a014aea0ccacc0b11f3e3d4
DeanLeong <dean.g.leong@gmail.com>
2021-03-30 14:53:12 -0400

    users and blogs seed, getting an error when characters go to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_155652) do
+ActiveRecord::Schema.define(version: 2021_03_30_184553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155652) do
     t.string "imgurl"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "characters", force: :cascade do |t|
