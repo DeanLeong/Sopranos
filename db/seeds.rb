39cf56395bd09616952594e16418a14de428a986
DeanLeong <dean.g.leong@gmail.com>
2021-03-30 14:16:22 -0400

    added auth controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Blog.destroy_all
+
+@admin = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
